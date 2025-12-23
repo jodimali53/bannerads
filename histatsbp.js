989465f99f2cea153e9101093bcc48912eabf270
jodimali53 <jodimali53@gmail.com>
2025-12-24 06:40:44 +0800

    Create histatsbp.js

---
+++ histatsbp.js
@@ -0,0 +1,12 @@
+var _Hasync = _Hasync || [];
+_Hasync.push(['Histats.start', '1,4998675,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+
+(function () {
+    var hs = document.createElement('script');
+    hs.type = 'text/javascript';
+    hs.async = true;
+    hs.src = '//s10.histats.com/js15_as.js';
+    (document.head || document.body).appendChild(hs);
+})();
