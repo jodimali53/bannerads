36d76f2ca8961a98f4fa59a746c514d2bc3706e2
jodimali53 <jodimali53@gmail.com>
2025-12-23 16:05:52 +0800

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,15 @@
+atOptions = {
+
+		'key' : 'cc6918fd801a992d9ba8c3111d0d6c68',
+
+		'format' : 'iframe',
+
+		'height' : 250,
+
+		'width' : 300,
+
+		'params' : {}
+
+	};
+
+	document.write('<scr' + 'ipt type="text/javascript" src="//hopefulvertical.com/cc6918fd801a992d9ba8c3111d0d6c68/invoke.js"></scr' + 'ipt>');
