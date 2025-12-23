0af7ed412bf193585e8bb74f1a4fc4652dc1333c
jodimali53 <jodimali53@gmail.com>
2025-12-23 21:12:55 +0800

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '902089f07b93fa2210e6ac91769ca93b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//hopefulvertical.com/902089f07b93fa2210e6ac91769ca93b/invoke.js"></scr' + 'ipt>');
